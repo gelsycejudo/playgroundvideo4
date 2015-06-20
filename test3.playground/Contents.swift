e258522eecade1612aedc33f3a315619c860725b
gelsycejudo <gelsycejudo@hotmail.com>
2015-06-20 11:49:55 -0500

    video4 de 10
    playground

---
+++ test3.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//func add(number1:Int, number2:Int) -> Int{
+//    return number1 + number2
+//}
+
+//add((1, 4),
+
+
+
+func getNames() -> (String, String){
+    
+    return ("name 1", "name 2");
+    
+}
+getNames();
